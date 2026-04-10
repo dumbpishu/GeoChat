2d403d8e0cf7529ded28e395d3f32b21eec6f894
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-04-11 02:42:14 +0530

    setup frontend with vite and tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="h-screen bg-pink-600">
+        <p className="text-white text-2xl text-center">Hello, This side priyanshu singh!</p>
+      </div>
+    </>
+  )
+}
+
+export default App

84787d7daea88f671cbd4151d2ae592c84671c2d
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-04-12 18:36:54 +0530

    add socket io in server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,26 @@ import { env } from "./config/env";
 import { connectDB } from "./config/db";
 import app from "./app";
 
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { initializeSocket } from "./realtime";
+
 const startServer = async () => {
     try {
         await connectDB();
         
-        app.listen(env.PORT, () => {
+        const httpServer = createServer(app);
+
+        const io = new Server(httpServer, {
+            cors: {
+                origin: env.NODE_ENV === "production" ? env.CROS_ORIGIN : "http://localhost:5173",
+                credentials: true
+            }
+        });
+
+        initializeSocket(io);
+
+        httpServer.listen(env.PORT, () => {
             console.log(`Server is running on port ${env.PORT}`);
         });
     } catch (error) {

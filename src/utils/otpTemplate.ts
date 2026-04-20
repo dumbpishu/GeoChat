e24c3a85e0ab85d396a108e7ad8be150361e1bbd
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-04-20 23:12:12 +0530

    update otp template

--- src/utils/otpTemplate.ts
+++ src/utils/otpTemplate.ts
@@ -1,15 +1,3 @@
-import crypto from "crypto";
-
-export const generateOtp = () => {
-    const otp = crypto.randomInt(100000, 999999).toString();
-    return otp;
-}
-
-export const hashOtp = (otp: string) => {
-    const hash = crypto.createHash("sha256").update(otp).digest("hex");
-    return hash;
-}
-
 export const otpTemplate = (otp: string) => {
     return `
 <!DOCTYPE html>

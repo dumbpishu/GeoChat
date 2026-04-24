9b4fabe77c96228d6341740556a304d854494503
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-04-24 23:18:22 +0530

    update precision area

--- backend/src/utils/room.ts
+++ backend/src/utils/room.ts
@@ -1,5 +1,5 @@
 import ngeohash from "ngeohash";
 
-export const getRoom = (latitude: number, longitude: number, precision: number = 4): string => {
+export const getRoom = (latitude: number, longitude: number, precision: number = 5): string => {
     return ngeohash.encode(latitude, longitude, precision);
 }
